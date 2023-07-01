87779fba9eeb899182046ecc63dcb0bb76384fef
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-06-30 23:39:46 -0300

    adicionando delete

---
+++ deletar.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'config.php';
+
+$id = filter_input(INPUT_GET, 'id');
+if($id){
+$sql = $pdo->prepare("DELETE FROM login WHERE id = :id");
+$sql->bindValue(':id', $id);
+$sql->execute();
+
+}
+
+header("Location: index.php");
+exit;
+?>
